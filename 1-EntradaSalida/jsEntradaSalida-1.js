d0b3bae559329cc33372412bd6a3a9d506978fb3
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-06-21 12:09:44 -0300

    Agrego archivos al repo

---
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -0,0 +1,8 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostrar()
+{
+    var $form = document.getElementById("FormIngreso");
+    
+    $form.innerHTML = "Esto funciona de maravilla";
+}
+
