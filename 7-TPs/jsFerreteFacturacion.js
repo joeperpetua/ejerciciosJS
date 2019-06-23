c78eafd4e917bc2c07174b6c70a83134cb43299c
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-06-23 15:04:30 -0300

    Subo primer TP terminado

--- 7-TPs/jsFerreteFacturacion.js
+++ 7-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,34 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+
+var $p1 = document.getElementById('PrecioUno');
+var $p2 = document.getElementById('PrecioDos');
+var $p3 = document.getElementById('PrecioTres');
+
+
+
+function Sumar(){
+    var p1 = parseInt($p1.value);
+    var p2 = parseInt($p2.value);
+    var p3 = parseInt($p3.value);
+
+    alert(p1+p2+p3);	
 }
-function Promedio () 
-{
-	
+
+function Promedio(){
+    var p1 = parseInt($p1.value);
+    var p2 = parseInt($p2.value);
+    var p3 = parseInt($p3.value);
+
+    alert((p1+p2+p3) / 3);
 }
-function PrecioFinal () 
-{
+
+function PrecioFinal(){
+    var p1 = parseInt($p1.value);
+    var p2 = parseInt($p2.value);
+    var p3 = parseInt($p3.value);
+
+    alert((p1+p2+p3) * 1.21);
 	
 }
