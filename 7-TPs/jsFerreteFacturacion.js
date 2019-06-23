4206d273d47125563fb4511ad8a7eb5d7a722abe
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-06-23 17:33:51 -0300

    Agrego descripcion de las funciones al TP1

--- 7-TPs/jsFerreteFacturacion.js
+++ 7-TPs/jsFerreteFacturacion.js
@@ -11,26 +11,29 @@ var $p3 = document.getElementById('PrecioTres');
 
 
 function Sumar(){
-    var p1 = parseInt($p1.value);
-    var p2 = parseInt($p2.value);
-    var p3 = parseInt($p3.value);
+    var p1 = parseFloat($p1.value);
+    var p2 = parseFloat($p2.value);
+    var p3 = parseFloat($p3.value);
+    var res = p1 + p2 + p3;
 
-    alert(p1+p2+p3);	
+    alert("Total sin IVA: $" + res);	
 }
 
 function Promedio(){
-    var p1 = parseInt($p1.value);
-    var p2 = parseInt($p2.value);
-    var p3 = parseInt($p3.value);
+    var p1 = parseFloat($p1.value);
+    var p2 = parseFloat($p2.value);
+    var p3 = parseFloat($p3.value);
+    var res = (p1+p2+p3) / 3;
 
-    alert((p1+p2+p3) / 3);
+    alert("Promedio de precios: $" + res);
 }
 
 function PrecioFinal(){
-    var p1 = parseInt($p1.value);
-    var p2 = parseInt($p2.value);
-    var p3 = parseInt($p3.value);
+    var p1 = parseFloat($p1.value);
+    var p2 = parseFloat($p2.value);
+    var p3 = parseFloat($p3.value);
+    var res = (p1+p2+p3) * 1.21;
 
-    alert((p1+p2+p3) * 1.21);
+    alert("Total con IVA: $" + res);
 	
 }
