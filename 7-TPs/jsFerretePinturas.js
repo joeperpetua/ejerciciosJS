5f79d7f24e8607ada128c3b739e37d4a25af6f5c
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-06-23 22:02:31 -0300

    Subo TP3 funcionando

--- 7-TPs/jsFerretePinturas.js
+++ 7-TPs/jsFerretePinturas.js
@@ -2,12 +2,19 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	
+
+var $temp = document.getElementById('Temperatura');
+
+function FahrenheitCentigrados (){
+    var temp = parseFloat($temp.value);
+
+    var res = ((temp - 32) / 1.8).toFixed(1) ;
+    alert(temp + "F° = " + res + "C°");
 }
 
-function CentigradosFahrenheit () 
-{
-	
+function CentigradosFahrenheit () {
+    var temp = parseFloat($temp.value);
+    
+    var res = ((temp * 1.8) + 32).toFixed(1);
+    alert(temp + "C° = " + res + "F°");
 }
