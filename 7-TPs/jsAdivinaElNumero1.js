036806195b511ff33d519b05d553edfaabd403fd
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-06-23 23:28:42 -0300

    Subo el TP5 funcionando

--- 7-TPs/jsAdivinaElNumero1.js
+++ 7-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,30 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
+
+var $guess = document.getElementById('numero');
+var $mostrar = document.getElementById('intentos');
 var numeroSecreto; 
-var contadorIntentos;
+var tries;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
-
+function comenzar(){
+  numeroSecreto = Math.floor(Math.random() * 100) + 1;
+  tries = 0;
+  $mostrar.value = tries;
+  $guess.value = 0;
+	alert("Se generó un nuevo numero a adivinar");
 }
 
-function verificar()
-{
-	
-	
+function verificar(){
+  var guess = parseInt($guess.value);
+  tries++;
+  $mostrar.value = tries;  
+
+  if(guess === numeroSecreto){
+    alert("ganaste en " + tries + " intentos troesma");
+  }else if(guess > numeroSecreto){
+    alert("te pasaste crack");
+  }else{
+    alert("cortina cortina, probá con un numero más grande");
+  }
 }
