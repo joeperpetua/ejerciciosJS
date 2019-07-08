95bda241e92329ca9cb8dc305df8c5dee635c414
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-07-08 20:35:42 -0300

    Agrego Tp 7 y TP 8 funcionando

--- 7-TPs/jsPiedarPapelTijera1.js
+++ 7-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,58 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
+var maquina;
+var humano;
 
-function comenzar()
-{
-	
+function comenzar(){
+    maquina = Math.floor( (Math.random() * 3 ) + 1);
+    alert("Se generó el numero de la wea");
+}
+
+function piedra(){
+    humano = 1;  
+    resultado(humano);  
+}
+
+function papel(){
+    humano = 2;
+    resultado(humano);
+}
+
+function tijera(){
+    humano = 3;
+    resultado(humano);
+}
+
+function resultado(humano){
+    if(humano == maquina){
+        alert("Empate " + humano + " - " + maquina);
+    }
+
+    if (humano === 1 && maquina === 2) {
+        alert("Derrota " + humano + " - " + maquina);
+    }
+
+    if (humano === 1 && maquina === 3) {
+        alert("Victoria " + humano + " - " + maquina);
+    }
+
+    if (humano === 2 && maquina === 1) {
+        alert("Victoria " + humano + " - " + maquina);
+    }
+
+    if (humano === 2 && maquina === 3) {
+        alert("Derrota " + humano + " - " + maquina);
+    }
+
+    if (humano === 3 && maquina === 1) {
+        alert("Derrota " + humano + " - " + maquina);
+    }
+
+    if (humano === 3 && maquina === 2) {
+        alert("Victoria " + humano + " - " + maquina);
+    }
 
 
-}//FIN DE LA FUNCIÓN
-function piedra()
-{
-	
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
-
-
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
-	
-
-}//FIN DE LA FUNCIÓN
+}
--- 7-TPs/jsPiedarPapelTijera2.js
+++ 7-TPs/jsPiedarPapelTijera2.js
@@ -1,37 +1,79 @@
 /*Ahora debemos informar cuantas veces se ganó,
  perdió o empató desde la última recarga de página.*/
-
-var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
-
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 3
-	 
+ 
+ var empates = 0;
+ var victorias = 0;
+ var derrotas = 0;
 
 
-}//FIN DE LA FUNCIÓN
-function piedra()
-{
-	
+var $showE = document.getElementById('empatadas');
+var $showG = document.getElementById('ganadas');
+var $showP = document.getElementById('perdidas');
 
+var maquina;
+var humano;
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
-	
-
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
-	
-
-}//FIN DE LA FUNCIÓN
-
-function mostarResultado()
-{
-
-
+function comenzar(){
+    maquina = Math.floor( (Math.random() * 3 ) + 1);
 }
+
+function piedra(){
+    humano = 1;  
+    resultado();  
+}
+
+function papel(){
+    humano = 2;
+    resultado();
+}
+
+function tijera(){
+    humano = 3;
+    resultado();
+}
+
+function resultado(){
+
+
+    if(humano == maquina){
+		alert("Empate");
+		empates++;
+    }
+
+    if (humano === 1 && maquina === 2) {
+		alert("Derrota");
+		derrotas++;
+    }
+
+    if (humano === 1 && maquina === 3) {
+		alert("Victoria");
+		victorias++;
+    }
+
+    if (humano === 2 && maquina === 1) {
+		alert("Victoria");
+		victorias++;
+    }
+
+    if (humano === 2 && maquina === 3) {
+		alert("Derrota");
+		derrotas++;
+    }
+
+    if (humano === 3 && maquina === 1) {
+		alert("Derrota");
+		derrotas++;
+    }
+
+    if (humano === 3 && maquina === 2) {
+		alert("Victoria");
+		victorias++;
+    }
+
+	$showE.value = empates;
+	$showG.value = victorias;
+	$showP.value = derrotas;
+	
+	comenzar();
+}
+
